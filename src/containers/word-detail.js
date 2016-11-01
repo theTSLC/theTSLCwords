8abc4802652bf5b98ee2342021052adadbf97ab7
Tom LeConey <tleconey@gmail.com>
2016-10-31 21:21:48 -0500

    hooked up word detail container to redux, took care of default before user selects first book, and successfully displaying

--- src/containers/word-detail.js
+++ src/containers/word-detail.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WordDetail extends Component {
-
+class WordDetail extends Component {
 	render() {
+		if (!this.props.word) {
+			return <div> Select a word to get started! </div>;
+		}
+		
 		return (
-			<div> Book Detail! </div>
-		)
+			<div> 
+				<h3> Details for: </h3>
+				<div> {this.props.word.spelling} </div>
+			</div>
+		);
 	}
 }
+
+function mapStateToProps(state) {
+	return {
+		word: state.activeWord
+	};
+}
+
+export default connect(mapStateToProps)(WordDetail)
