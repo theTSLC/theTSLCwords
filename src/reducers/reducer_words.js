cacb24cff5b2ea9f66c49d8d39a1522bfbbcdb52
Tom LeConey <tleconey@gmail.com>
2016-10-31 21:34:10 -0500

    fleshing out word details in line with wireframed site design basic principles, as pertaining to MVP

--- src/reducers/reducer_words.js
+++ src/reducers/reducer_words.js
@@ -1,13 +1,16 @@
 export default function() {
 	return [
-		{spelling: "bon vivant"},
-		{spelling: "denouement"},
-		{spelling: "parthenogenesis"},
-		{spelling: "ineffable"},
-		{spelling: "savoir fare"},
-		{spelling: "equanimity"},
-		{spelling: "inimitable"},
-		{spelling: "polymath"},
-		{spelling: "voluble"}
+		{spelling:'bon vivant', 
+		 definition: 'a person having cultivated, refined, and sociable tastes especially with respect to food and drink and company',
+		 example: '',
+		 etymology: 'French: literally "good liver", as in one who lives (First Known Use: circa 1695)'},
+		{spelling:'denouement',
+	   definition: ''},
+		{spelling:'ineffable'},
+		{spelling:'savoir fare'},
+		{spelling:'equanimity'},
+		{spelling:'inimitable'},
+		{spelling:'polymath'},
+		{spelling:'voluble'}
 	]
 }
